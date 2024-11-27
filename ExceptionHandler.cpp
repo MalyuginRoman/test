c2b22927f24795ff2398c2cf0f3ec0b0f9fd6203
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-11-27 14:44:03 +0500

    Create ExceptionHandler.cpp

---
+++ ExceptionHandler.cpp
@@ -0,0 +1,45 @@
+#include "ExceptionHandler.h"
+#include "CommandLoger.h"
+
+class ExceptionHandlerP
+{
+public:
+    ICommand* cmd;
+    std::exception ex;
+    ExceptionHandlerP(ICommand* cmd, std::exception ex) :
+        cmd(0),
+        ex(ex)
+    {
+    }
+};
+
+ExceptionHandler::ExceptionHandler(ICommand* cmd, std::exception ex) :
+    imp(new ExceptionHandlerP(cmd, ex))
+{
+}
+
+ExceptionHandler::~ExceptionHandler()
+{
+    delete imp;
+}
+
+void ExceptionHandler::executeRepeat(ExceptionHandler* handler, CommandQueue* cmd, ICommand* com, std::exception ex)
+{
+    std::cout << "Repeat execute of ExceptionHandler" << ex.what() << std::endl;
+    cmd->add(com);
+    cmd->del();
+    try {
+        cmd->front()->execute();
+        cmd->del();
+    } catch( std::exception ex1) {
+        handler->executeWrite(cmd, cmd->front(), ex1);
+    }
+}
+
+void ExceptionHandler::executeWrite(CommandQueue* cmd, ICommand* com, std::exception ex)
+{
+    std::cout << "Write execute of ExceptionHandler" << ex.what() << std::endl;
+    CommandLoger *cmd_loger = new CommandLoger;
+    cmd_loger->execute();
+    cmd->del();
+}
