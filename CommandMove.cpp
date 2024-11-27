77f52ba599327631908e55cc2ff614d11818b64b
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-11-27 14:47:14 +0500

    Create CommandMove.cpp

---
+++ CommandMove.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "CommandMove.h"
+
+CommandMove::CommandMove()
+{
+    this->getStr("CommandMove");
+}
+
+void CommandMove::execute()
+{
+    std::cout << "Start execute of CommandMove" << std::endl;
+    throw std::runtime_error ("CommandMove");
+}
+
+void CommandMove::getStr(std::string value)
+{
+    this->str = value;
+}
+
+std::string CommandMove::setStr()
+{
+    return this->str;
+}
